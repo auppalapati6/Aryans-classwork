f984a3b1cb8f500f30a7f73da0c856b4a52e5bbb
auppalapati6 <85558299+auppalapati6@users.noreply.github.com>
2021-11-11 10:18:43 +0000

    s

--- subroutines/subroutines task 1.py
+++ subroutines/subroutines task 1.py
@@ -1,17 +1,21 @@
 def test1():
-    print("hellow")
+    print("hello")
 #end procedurea
 
 test1()
 
-def multiples(table, startnum, endnum, name):
-    name = input("what is your name")
-    print("hello", name)
-    print("enter table, starting number and ending number")
-    table = int(input())
-    startnum = int(input())
-    endnum = int(input())
-    for x in range(startnum,endnum+1):
-      print((x*table))
+print("what is your name")    
+Name = input() 
+print("enter table, starting number and ending number")
+Table = input()
+Startnum = input()
+Endnum = input()
 
+def multiples() : 
+   print("hi",Name)
+   for x in range(Startnum,Endnum+1):
+       print("your talble is,", Table, "start number is", Startnum, "ending number is", Endnum, x*Table)
 multiples()
+
+
+
