f15b08691bd16ce76719df331a8489ce18ba6cf5
auppalapati6 <85558299+auppalapati6@users.noreply.github.com>
2021-11-12 08:54:53 +0000

    iteration

--- iteration/iteration2.py
+++ iteration/iteration2.py
@@ -3,9 +3,8 @@ pasword = "tues1212"
 counter = 0
 while counter<3:
     attempt=input()
-    if attempt=password:
+    if attempt == pasword:
        print("password accepted")
-    elif attempt !=password:
+    elif attempt !=pasword:
         print("password rejected")
         counter = counter+1
-endwhile
